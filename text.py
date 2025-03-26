09cacef7e40faba2a376c847715a1185affd1ab5
rajupalemsushanth <rajupalem.sushanth@s.amity.edu>
2025-03-26 13:34:10 +0530

    Update text.py

--- text.py
+++ text.py
@@ -1,1 +1,10 @@
-i am sushanth
+# This program adds two numbers
+
+num1 = 8.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
